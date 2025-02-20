f55b15d9ee34318e716cdffa06cf339baca173f1
souta <kansouta1125@icloud.com>
2025-02-21 01:01:12 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 const btn = document.getElementById("executeStorage");
+const local = document.getElementById("local");
+const session = document.getElementById("session");
+
+local.innerText = localStorage.getItem("name") || "No data found";
+session.innerText = sessionStorage.getItem("name") || "No data found";
 
 btn.addEventListener("click", () => {
   // sessionStorage
